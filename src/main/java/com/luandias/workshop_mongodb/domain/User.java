cefba24b183128bfd1608dc1c1745f669db8b10c
Luan Dias <luandsvt@gmail.com>
2024-10-18 15:41:39 -0300

    Fix ID(Long to String), Creation class Instantiation

--- src/main/java/com/luandias/workshop_mongodb/domain/User.java
+++ src/main/java/com/luandias/workshop_mongodb/domain/User.java
@@ -12,7 +12,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	private Long id;
+	private String id;
 	private String name;
 	private String email;
 
@@ -20,18 +20,18 @@ public class User implements Serializable {
 
 	}
 
-	public User(Long id, String name, String email) {
+	public User(String id, String name, String email) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
-	public Long getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
--- src/main/java/com/luandias/workshop_mongodb/domain/repository/UserRepository.java
+++ src/main/java/com/luandias/workshop_mongodb/domain/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.luandias.workshop_mongodb.domain.User;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
 
 }
