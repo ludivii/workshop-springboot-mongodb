78c4bfdacfb53855c9094f06d8d10f24de7836ea
Luan Dias <luandsvt@gmail.com>
2024-10-18 17:33:12 -0300

    AuthorDTO

--- src/main/java/com/luandias/workshop_mongodb/config/Instantiation.java
+++ src/main/java/com/luandias/workshop_mongodb/config/Instantiation.java
@@ -12,37 +12,38 @@ import com.luandias.workshop_mongodb.domain.Post;
 import com.luandias.workshop_mongodb.domain.User;
 import com.luandias.workshop_mongodb.domain.repository.PostRepository;
 import com.luandias.workshop_mongodb.domain.repository.UserRepository;
+import com.luandias.workshop_mongodb.dto.AuthorDTO;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-	
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
-	
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
-		postRepository.saveAll(Arrays.asList(post1,post2));
-		
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
 
-	
 }
