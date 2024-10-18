c1b7867dad42c56e3c45290d6e14d5271535cd60
Luan Dias <luandsvt@gmail.com>
2024-10-18 17:52:57 -0300

    PostService and PostResource

---
+++ src/main/java/com/luandias/workshop_mongodb/servicies/PostService.java
@@ -0,0 +1,29 @@
+package com.luandias.workshop_mongodb.servicies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luandias.workshop_mongodb.domain.Post;
+import com.luandias.workshop_mongodb.domain.repository.PostRepository;
+import com.luandias.workshop_mongodb.servicies.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> Post = repo.findById(id);
+		return Post.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+
+	}
+
+}
