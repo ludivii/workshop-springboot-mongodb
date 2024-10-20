51fa0c6f326fb8cb8f44b50c4c542bec853f0407
Luan <luandsvt@gmail.com>
2024-10-20 11:48:06 -0300

    Simple search w/ @query

--- src/main/java/com/luandias/workshop_mongodb/domain/repository/PostRepository.java
+++ src/main/java/com/luandias/workshop_mongodb/domain/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.luandias.workshop_mongodb.domain.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.luandias.workshop_mongodb.domain.Post;
@@ -12,4 +13,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 }
